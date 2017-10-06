95d2864dd86e05c7aa94f93f5490f87ea56198ce
Tuan <tuan.tran1122@gmai.com>
2017-10-05 23:36:38 -0500

    Add new content

--- models/share.php
+++ models/share.php
@@ -3,6 +3,6 @@ class ShareModel extends Model{
 	public function Index(){
 		$this->query('SELECT * FROM shares');
 		$rows = $this->resultSet();
-		print_r($rows);
+		return $rows;
 	}
 }
---
+++ views/shares/index.php
@@ -0,0 +1,14 @@
+<div>
+	<a class="btn btn-success btn-share" href="<?php echo ROOT_PATH; ?>shares/add">Say Something</a>
+	<?php foreach($viewmodel as $item) : ?>
+		<div class="well">
+			<h3><?php echo $item['title']; ?></h3>
+			<small><?php echo $item['create_date']; ?></small>
+			<hr />
+			<p><?php echo $item['body']; ?></p>
+			<br />
+			<a class="btn btn-default" href="<?php echo $item['link']; ?>" target="_blank">Weblink</a> 
+		</div>
+
+	<?php endforeach; ?>
+</div>
